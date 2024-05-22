fb6aa7189d22204fa2c2dc6f02de84303ae01115
vibe78 <victornwogu78@yahoo.com>
2024-05-22 15:06:02 +0100

    First Commit

---
+++ doc/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import (
+    Book_Apointment_model,
+    Decline_note,
+    Medical_test,
+    Send_report_to_pharmacy,
+    confirm_drug,
+    chat_model,
+    Notification,
+    NurseModels,
+    Vital_Signs,
+)
+# Register your models here.
+
+
+admin.site.register(Book_Apointment_model)
+admin.site.register(Decline_note)
+admin.site.register(Medical_test)
+admin.site.register(Send_report_to_pharmacy)
+admin.site.register(confirm_drug)
+
+admin.site.register(chat_model)
+
+admin.site.register(Notification)
+
+
+admin.site.register(NurseModels)
+admin.site.register(Vital_Signs)
